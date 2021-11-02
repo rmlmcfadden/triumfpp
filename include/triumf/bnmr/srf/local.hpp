cbc2496b4dd6d3fb3e5c0de8986f516b536cf116
Ryan M. L. McFadden <rmlm@triumf.ca>
2021-11-02 12:55:55 -0700

    add case of thin film for the "local" analysis model
    - make a seperate member function for reading the SRIM profile fit parameters
    - don't use inheretance to define the new class (because I couldn't get it working...)

--- include/triumf/bnmr/srf/local.hpp
+++ include/triumf/bnmr/srf/local.hpp
@@ -67,21 +67,45 @@ T slr_rate_z(T z, T temperature, T critical_temperature, T lambda_0, T exponent,
   }
 }
 
+template <typename T = double>
+T slr_rate_film_z(T z, T temperature, T critical_temperature, T lambda_0,
+                  T exponent, T applied_field, T dipole_field,
+                  T correlation_rate, T slr_constant, T slr_exponent,
+                  T surface_thickness, T surface_rate, T film_thickness) {
+  // correct depth for the surface layer
+  T _z_ = z - surface_thickness;
+  // correct film thickness for the surface layer
+  T _d_ = 0.5 * (film_thickness - surface_thickness);
+  if (_z_ < 0.0) {
+    return surface_rate;
+  } else {
+    // calculate the local field from the screening profile
+    T lambda = triumf::superconductivity::phenomenology::penetration_depth(
+        temperature, critical_temperature, exponent, lambda_0);
+    T screened_field = temperature > critical_temperature
+                           ? applied_field
+                           : applied_field *
+                                 std::cosh((0.5 * _d_ - _z_) / lambda) /
+                                 std::cosh(0.5 * _d_ / lambda);
+    // calculate the dipole-dipole SLR rate in the superconducting state
+    T dd_rate = triumf::nmr::dipole_dipole::slr_rate<T>(
+        screened_field, dipole_field, correlation_rate,
+        triumf::bnmr::nuclei::lithium_8<T>::gyromagnetic_ratio(),
+        triumf::nmr::nuclei::niobium_93<T>::gyromagnetic_ratio());
+    // calculate the SLR rate in the normal state
+    T ns_rate = slr_constant * std::pow(temperature, slr_exponent);
+    // return the "surface" contribution at shallow depths
+    return dd_rate + ns_rate;
+  }
+}
+
 /// depth-resolved analyzer
 template <typename T = double> class DepthResolvedAnalyzer {
 public:
   /// constructor.
   DepthResolvedAnalyzer(const std::string &csv_filename) {
-    // read the data into a ROOT DataFrame...
-    auto df = ROOT::RDF::MakeCsvDataFrame(csv_filename);
-    // ...and extract the values
-    _energy = df.Take<T>("Energy (keV)").GetValue();
-    _alpha = df.Take<T>("Alpha").GetValue();
-    _alpha_error = df.Take<T>("Alpha Error").GetValue();
-    _beta = df.Take<T>("Beta").GetValue();
-    _beta_error = df.Take<T>("Beta Error").GetValue();
-    _z_max = df.Take<T>("Max (nm)").GetValue();
-    _z_max_error = df.Take<T>("Max Error (nm)").GetValue();
+    // read in the data for the stopping profiles
+    read_csv_data(csv_filename);
 
     // default initialized values
     temperature = 2.5;
@@ -99,13 +123,27 @@ public:
     n_bins = 201;
   };
 
+  /// Read the CSV data
+  void read_csv_data(const std::string &csv_filename) {
+    // read the data into a ROOT DataFrame...
+    auto df = ROOT::RDF::MakeCsvDataFrame(csv_filename);
+    // ...and extract the values
+    _energy = df.Take<T>("Energy (keV)").GetValue();
+    _alpha = df.Take<T>("Alpha").GetValue();
+    _alpha_error = df.Take<T>("Alpha Error").GetValue();
+    _beta = df.Take<T>("Beta").GetValue();
+    _beta_error = df.Take<T>("Beta Error").GetValue();
+    _z_max = df.Take<T>("Max (nm)").GetValue();
+    _z_max_error = df.Take<T>("Max Error (nm)").GetValue();
+  };
+
   /// Return the the minium energy available for interpolation.
   T energy_min() {
     return *std::min_element(_energy.begin(), _energy.end()) +
            std::sqrt(std::numeric_limits<T>::epsilon());
   };
 
-  /// Return the the maximum energy availalbe for interpolation.
+  /// Return the the maximum energy available for interpolation.
   T energy_max() {
     return *std::max_element(_energy.begin(), _energy.end()) -
            std::sqrt(std::numeric_limits<T>::epsilon());
@@ -210,6 +248,130 @@ public:
   T surface_thickness;
   T surface_rate;
   T electron_phonon_coupling;
+
+private:
+  /// vectors of data from csv file
+  std::vector<T> _energy;
+  std::vector<T> _alpha;
+  std::vector<T> _alpha_error;
+  std::vector<T> _beta;
+  std::vector<T> _beta_error;
+  std::vector<T> _z_max;
+  std::vector<T> _z_max_error;
+  /// number of bins + 1 used in the "histogram" summation
+  std::size_t n_bins;
+};
+
+/// depth-resolved analyzer
+template <typename T = double> class DepthResolvedFilmAnalyzer {
+public:
+  /// constructor.
+  DepthResolvedFilmAnalyzer(const std::string &csv_filename) {
+    // read in the data for the stopping profiles
+    read_csv_data(csv_filename);
+
+    // default initialized values
+    temperature = 2.5;
+    critical_temperature = 9.25;
+    lambda_0 = 40.0;
+    exponent = 4.0;
+    applied_field = 0.02;
+    dipole_field = 1e-5;
+    correlation_rate = 1.0 / 23.8e-6;
+    slr_constant = 0.75;
+    slr_exponent = 1.0;
+    surface_thickness = 5.0;
+    surface_rate = 10.0;
+    film_thickness = 300.0;
+    //
+    n_bins = 201;
+  };
+
+  /// Read the CSV data
+  void read_csv_data(const std::string &csv_filename) {
+    // read the data into a ROOT DataFrame...
+    auto df = ROOT::RDF::MakeCsvDataFrame(csv_filename);
+    // ...and extract the values
+    _energy = df.Take<T>("Energy (keV)").GetValue();
+    _alpha = df.Take<T>("Alpha").GetValue();
+    _alpha_error = df.Take<T>("Alpha Error").GetValue();
+    _beta = df.Take<T>("Beta").GetValue();
+    _beta_error = df.Take<T>("Beta Error").GetValue();
+    _z_max = df.Take<T>("Max (nm)").GetValue();
+    _z_max_error = df.Take<T>("Max Error (nm)").GetValue();
+  };
+
+  /// Return the the minium energy available for interpolation.
+  T energy_min() {
+    return *std::min_element(_energy.begin(), _energy.end()) +
+           std::sqrt(std::numeric_limits<T>::epsilon());
+  };
+
+  /// Return the the maximum energy available for interpolation.
+  T energy_max() {
+    return *std::max_element(_energy.begin(), _energy.end()) -
+           std::sqrt(std::numeric_limits<T>::epsilon());
+  };
+
+  /// Return an interpolated alpha value.
+  T alpha(T energy_keV) {
+    static boost::math::interpolators::pchip<std::vector<T>> alpha_interpolator(
+        std::move(std::vector<T>(_energy)), std::move(std::vector<T>(_alpha)));
+    return alpha_interpolator(energy_keV);
+  };
+
+  /// Return an interpolated beta value.
+  T beta(T energy_keV) {
+    static boost::math::interpolators::pchip<std::vector<T>> beta_interpolator(
+        std::move(std::vector<T>(_energy)), std::move(std::vector<T>(_beta)));
+    return beta_interpolator(energy_keV);
+  };
+
+  /// Return an interpolated z_max value.
+  T z_max(T energy_keV) {
+    static boost::math::interpolators::pchip<std::vector<T>> z_max_interpolator(
+        std::move(std::vector<T>(_energy)), std::move(std::vector<T>(_z_max)));
+    return z_max_interpolator(energy_keV);
+  };
+
+  /// Return the average implantation depth.
+  T z_average(T energy_keV) {
+    T a = alpha(energy_keV);
+    T b = beta(energy_keV);
+    T zm = z_max(energy_keV);
+    return zm * a / (a + b);
+  };
+
+  /// depth-averaging using numeric integration
+  T operator()(T energy_keV) {
+    static boost::math::quadrature::tanh_sinh<T> integrator;
+    auto integrand = [&](T z) {
+      return slr_rate_film_z<T>(z, temperature, critical_temperature, lambda_0,
+                                exponent, applied_field, dipole_field,
+                                correlation_rate, slr_constant, slr_exponent,
+                                surface_thickness, surface_rate,
+                                film_thickness) *
+             triumf::srim::pdf::modified_beta<T>(
+                 z, alpha(energy_keV), beta(energy_keV), z_max(energy_keV));
+    };
+    T Q = integrator.integrate(integrand, 0.0, z_max(energy_keV));
+    return Q;
+  };
+
+  /// model parameters
+  T temperature;
+  T critical_temperature;
+  T lambda_0;
+  T exponent;
+  T applied_field;
+  T dipole_field;
+  T correlation_rate;
+  T slr_constant;
+  T slr_exponent;
+  T surface_thickness;
+  T surface_rate;
+  T electron_phonon_coupling;
+  T film_thickness;
 
 private:
   /// vectors of data from csv file
