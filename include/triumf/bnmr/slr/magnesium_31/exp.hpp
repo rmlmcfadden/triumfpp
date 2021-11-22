ec3159132c8cc56478ff81d325c2c05f27eca6d3
Ryan M. L. McFadden <rmlm@triumf.ca>
2021-11-21 21:16:19 -0800

    clean up pulsed exponential for 31Mg
    - clarify calls the similarly named functions
    - explicitly handle cases for different times

--- include/triumf/bnmr/slr/magnesium_31/exp.hpp
+++ include/triumf/bnmr/slr/magnesium_31/exp.hpp
@@ -19,16 +19,27 @@ namespace magnesium_31 {
 /// pulsed exponential with magnesium-31 decay corrections
 template <typename T = double>
 T pulsed_exp(T time, T nuclear_lifetime, T pulse_length, T asymmetry,
-                  T slr_rate) {
-  double beam_pulse = 1e6;
-  return fa_31Mg(time, pulse_length, beam_rate) *
-         pulsed_exp<T>(time, nuclear_lifetime, pulse_length, asymmetry,
-                       slr_rate);
+             T slr_rate) {
+  // explicitly return the initial asymmetry at time = 0.0
+  // because the "activity" function returns a NaN at that time
+  if (time == 0.0) {
+    return asymmetry;
+    // use the "usual" evaluation approach for finite positive times
+  } else if (time > 0.0) {
+    T beam_rate = 1e6;
+    return fa_31Mg(time, pulse_length, beam_rate) *
+           triumf::bnmr::slr::pulsed_exp<T>(time, nuclear_lifetime,
+                                            pulse_length, asymmetry, slr_rate);
+    // return zero for negative times
+  } else {
+    return 0.0;
+  }
 }
 
 /// pulsed exponential with magnesium-31 decay corrections (ROOT)
 template <typename T = double> T pulsed_exp(const T *x, const T *par) {
-  return pulsed_exp<T>(*x, par[0], par[1], par[2], par[3]);
+  return triumf::bnmr::slr::magnesium_31::pulsed_exp<T>(*x, par[0], par[1],
+                                                        par[2], par[3]);
 }
 
 } // namespace magnesium_31
